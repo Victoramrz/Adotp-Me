c03ac7f93c97e9efbf5fd9b36492c465502a7908
Victor <ramirezvictorman@gmail.com>
2023-02-28 10:00:44 +0000

    Initial commit

---
+++ Adopt Me/Adopt_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adopt_MeApp.swift
+//  Adopt Me
+//
+//  Created by CIFP Villa De Aguimes on 28/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Adopt_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adopt Me/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Adopt Me
+//
+//  Created by CIFP Villa De Aguimes on 28/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
