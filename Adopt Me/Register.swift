c78ed2b7d4213e947528af2da3317e218d61c792
Victoramrz <ramirezvictorman@hotmail.com>
2023-03-06 18:18:53 +0000

    Segunda escena inicio

---
+++ Adopt Me/Register.swift
@@ -0,0 +1,20 @@
+//
+//  Register.swift
+//  Adopt Me
+//
+//  Created by CIFP Villa De Aguimes on 6/3/23.
+//
+
+import SwiftUI
+
+struct Register: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Register_Previews: PreviewProvider {
+    static var previews: some View {
+        Register()
+    }
+}
